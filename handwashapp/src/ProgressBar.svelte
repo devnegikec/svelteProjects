5bc59a2355c9791e35d444e9c34c7168c5603b88
Devendra Negi <devnegikec@gmail.com>
2021-09-15 15:13:45 +0530

    App

---
+++ handwashapp/src/ProgressBar.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let progress = 30;
+</script>
+<div bp="grid">
+    <div class="progress-container" bp="offset-5@md 4@md 12@sm">
+        <div class="progress-bar" style="width: {progress}%"></div>
+    </div>
+</div>
+
+<style>
+    .progress-container {
+        margin: 20px 0;
+        border: solid rgb(194, 194, 194) 1px;
+    }
+    .progress-bar {
+        height: 30px;
+        background-color: rgb(77, 199, 55);
+    }
+</style>
